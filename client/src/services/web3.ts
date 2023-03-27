9ae7c2656fa6215f7043821875bfe183b28cdd7b
olaysco <olayiwolaodunsi@gmail.com>
2023-03-27 06:10:22 +0100

    Refactor how web3 instance is instantiated
    This is beacuase window.web3 has been depreceated in MetaMask, and will be removed completely soon

--- client/src/services/web3.ts
+++ client/src/services/web3.ts
@@ -1,30 +1,22 @@
-import { isPromise } from '@vue/shared'
 import Web3 from 'web3'
 declare let window: any
 
-export const getWeb3 = new Promise(function (resolve: (value: { web3(): Web3 }) => void, reject) {
-  //  First check if web3js was injected with metamask.
-  const web3js = window.web3
-  let web3: Web3
-  if (typeof web3js !== 'undefined') {
-    web3 = new Web3(window.ethereum)
-  } else {
-    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'))
-  }
-  const request = window.ethereum.request({ method: 'eth_requestAccounts' })
-  if (isPromise(request)) {
-    request.then(() => {
-      resolve({
-        web3() {
-          return web3
-        }
-      })
-    })
-  }
-
-  resolve({
-    web3() {
-      return web3
+export const getWeb3 = async () => {
+  //  First check if ethereum is injected.
+  let provider: any
+  if (typeof window.ethereum !== 'undefined') {
+    provider = window.ethereum
+    try {
+      await window.ethereum.enable()
+    } catch (error) {
+      console.error('User denied access')
     }
-  })
-})
+  } else if (window.web3) {
+    provider = window.web3.currentProvider
+  } else {
+    provider = new Web3.providers.HttpProvider('http://localhost:9545')
+  }
+  const web3 = new Web3(provider)
+
+  return { provider, web3 }
+}
